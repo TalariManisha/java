35deccac5b047805b6c4345d74724b5cb5595680
talari manisha <talarimanisha2020@gmail.com>
2024-02-16 15:02:07 +0530

    adding java files

---
+++ vari.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+class vari
+{    public static void main(String args[])
+     {  int x=5;
+        float y=22.4f;
+        byte m=1;
+        boolean g=true;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(g);
+        System.out.println(m);
+        System.out.println("int max:"+Integer.MAX_VALUE);
+        System.out.println("int mIN:"+Integer.MIN_VALUE);
+        System.out.println("bytes:"+Integer.BYTES);
+        System.out.println("FLOAT max:"+Float.MAX_VALUE);
+        System.out.println("Float max:"+Float.MIN_VALUE);
+        System.out.println("FLOAT BYTES:"+Float.BYTES);
+        System.out.println("BYTES mIN:"+Byte.MIN_VALUE);
+        System.out.println("BYTES MAX:"+Byte.MAX_VALUE);
+        System.out.println("BYTES:"+Byte.BYTES);
+     }
+}
